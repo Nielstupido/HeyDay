e706cb5bee1220549592b57e8877ccfa7b38edb8
nanikaa <130446747+nanikaa@users.noreply.github.com>
2023-08-30 17:57:33 +0800

    Music, Mechanics, Credits
    -Mechanics, Credits, and settings overlay
    - Music and SFX toggle and slider

--- Assets/Scripts/MainMenu/MainMenuManager.cs
+++ Assets/Scripts/MainMenu/MainMenuManager.cs
@@ -8,6 +8,12 @@ public class MainMenuManager : MonoBehaviour
     [SerializeField] private GameObject startBtn;
     [SerializeField] private GameObject exitBtn;
     [SerializeField] private GameObject settingsBtn;
+    [SerializeField] private GameObject mechanicsBtn;
+    [SerializeField] private GameObject creditsBtn;
+    [SerializeField] private GameObject mechanicsPanel;
+    [SerializeField] private GameObject creditsPanel;
+    [SerializeField] private GameObject settingsPanel;
+    [SerializeField] private GameObject closeBtn;
 
 
     private void Start()
@@ -20,6 +26,8 @@ public class MainMenuManager : MonoBehaviour
     {
         LeanTween.moveY(startBtn, (float)(Screen.height / 3.5), 1.2f).setEaseOutElastic().delay = 0.2f;
         LeanTween.moveY(exitBtn, (float)(Screen.height / 8.5), 1.2f).setEaseOutElastic().setOnComplete(StartSpawn).delay = 0.4f;
+        LeanTween.moveY(mechanicsBtn, (float)(Screen.height / 4.5), 1.2f).setEaseOutElastic().setOnComplete(StartSpawn).delay = 0.2f;
+        LeanTween.moveY(creditsBtn, (float)(Screen.height / 4.5), 1.2f).setEaseOutElastic().setOnComplete(StartSpawn).delay = 0.2f;
     }
 
 
@@ -36,6 +44,29 @@ public class MainMenuManager : MonoBehaviour
         SceneManager.LoadScene("MainGame");
     }
 
+    public void ShowGameMechanics()
+    {
+        mechanicsPanel.SetActive(true);
+        AudioManager.Instance.PlaySFX("Select");
+    }
+
+    public void ShowGameCredits()
+    {
+        creditsPanel.SetActive(true);
+        AudioManager.Instance.PlaySFX("Select");
+    }
+
+    public void ShowSettings()
+    {
+        settingsPanel.SetActive(true);
+        AudioManager.Instance.PlaySFX("Select");
+    }
+
+    public void ExitSettings()
+    {
+        settingsPanel.SetActive(false);
+        //AudioManager.Instance.PlaySFX("Select");
+    }
 
     public void ExitGame()
     {
