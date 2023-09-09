a5eb0fea3f36b2bd2724c8433f702f9e4f8cb567
nanikaa <130446747+nanikaa@users.noreply.github.com>
2023-09-10 00:45:09 +0800

    Stats Checker, Hospitalization, Clock Time
    Stats Checker - checks if any of 3 stats bar reaches the lower limit.
    
    Hospitalization - activates when a stat reaches limit. Number of hospitalization days depends on sum of all current stats. Number of days will be multiplied to hospital fee and the total hospital bill is deducted from current MONEY stats.
    
    Clock Time - adds a specific amount of time to current clock value for every action. 12hr format. No AM/PM label yet.

--- Assets/Scripts/MainGame/Player/Player.cs
+++ Assets/Scripts/MainGame/Player/Player.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 
 public enum PlayerStats
@@ -12,12 +13,19 @@ public enum PlayerStats
     MONEY
 }
 
-
-
 public class Player : MonoBehaviour
 {
     private IDictionary<PlayerStats, float> playerStatsDict = new Dictionary<PlayerStats, float>();
     private string playerName;
+    private float hospitalFee = 1500; // hospital fee per day
+    private float numOfdays = 0;
+    private float currentTime = 7; //time will start at 7AM
+    
+    [SerializeField] private GameObject hospitalizedPrompt;
+    [SerializeField] private TextMeshProUGUI daysHospitalized;
+    [SerializeField] private TextMeshProUGUI totalBill;
+    [SerializeField] private TextMeshProUGUI clockValue;
+    [SerializeField] private TextMeshProUGUI indicatorAMPM;
 
 
     public string PlayerName { set{playerName = value;} get{return playerName;}}
@@ -28,7 +36,7 @@ public class Player : MonoBehaviour
         playerStatsDict.Add(PlayerStats.HAPPINESS, 100f);
         playerStatsDict.Add(PlayerStats.HUNGER, 100f);
         playerStatsDict.Add(PlayerStats.ENERGY, 100f);
-        playerStatsDict.Add(PlayerStats.MONEY, 100f);
+        playerStatsDict.Add(PlayerStats.MONEY, 5000f);
     }
 
 
@@ -65,6 +73,11 @@ public class Player : MonoBehaviour
 
     public void Walk(float energyLevelCutValue)
     {
+        AddClockTime(.3f);
+        StatsChecker(playerStatsDict[PlayerStats.ENERGY], playerStatsDict[PlayerStats.HAPPINESS], playerStatsDict[PlayerStats.HUNGER]);
+        /*Debug.Log("Energy:" + playerStatsDict[PlayerStats.ENERGY]);
+        Debug.Log("Happiness:" + playerStatsDict[PlayerStats.HAPPINESS]);
+        Debug.Log("Money:" + playerStatsDict[PlayerStats.MONEY]);*/
         playerStatsDict[PlayerStats.ENERGY] -= energyLevelCutValue;
         PlayerStatsObserver.onPlayerStatChanged(PlayerStats.ENERGY, playerStatsDict);
     }
@@ -74,5 +87,102 @@ public class Player : MonoBehaviour
     {
         playerStatsDict[PlayerStats.ENERGY] -= energyLevelCutValue;
         PlayerStatsObserver.onPlayerStatChanged(PlayerStats.ENERGY, playerStatsDict);
+    }
+
+    public void StatsChecker(float currentEnergy, float currentHappiness, float currentHunger)
+    {
+        if (currentEnergy <= 10 | currentHappiness <= 10 | currentHunger <= 10)//checks if any of the stats reaches lower limit
+        {
+            float sumOfStats = currentEnergy + currentHappiness + currentHunger;
+
+            if ((sumOfStats*100) / 300 <= 10)
+            {
+                Hospitalized(5);
+            }
+            else if ((sumOfStats*100) / 300 <= 30)
+            {
+                Hospitalized(4);
+            }
+            else if ((sumOfStats*100) / 300 <= 50)
+            {
+                Hospitalized(3);
+            }
+            else if ((sumOfStats*100) / 300 <= 70)
+            {
+                Hospitalized(2);
+            }
+            
+        }
+    }
+
+    public void Hospitalized(float dayCount)
+    {
+        hospitalizedPrompt.SetActive(true);
+        daysHospitalized.text = dayCount.ToString();
+        float bill = hospitalFee*dayCount;
+        totalBill.text = bill.ToString();
+
+        numOfdays = dayCount;
+    }
+    
+    public void PayHospitalFees()
+    {
+        playerStatsDict[PlayerStats.MONEY] -= numOfdays*hospitalFee;
+        playerStatsDict[PlayerStats.HAPPINESS] = 100;
+        playerStatsDict[PlayerStats.ENERGY] = 100;
+        playerStatsDict[PlayerStats.HUNGER] = 100;
+
+        PlayerStatsObserver.onPlayerStatChanged(PlayerStats.ALL, playerStatsDict);
+        //Debug.Log("Money after hospi:" + playerStatsDict[PlayerStats.MONEY]);
+
+        hospitalizedPrompt.SetActive(false);
+    }
+
+    public void AddClockTime(float addedClockValue)
+    {
+        currentTime += addedClockValue;
+        int minutes = 0;
+        int hours = 0; 
+        //Debug.Log("Current Time = " + currentTime);
+        //clockValue.text = currentTime.ToString() + ":00";
+
+        if (currentTime % 1 != 0)
+        {
+            Debug.Log("is FLOAT");
+            hours = (int)currentTime;
+            float temp = (currentTime % 1) * 100;
+            minutes = (int)temp;
+            Debug.Log("Hours = " + hours);
+            Debug.Log("Minutes = " + minutes);
+
+            if (minutes >= 60)
+            {
+                minutes = 0;
+                hours ++;
+
+                if (hours > 12)
+                {
+                    hours = 1;
+                }
+                clockValue.text = hours.ToString() + ":00";
+                currentTime = hours;
+            }
+            else if (hours > 12)
+            {
+                hours = 1;
+                clockValue.text = hours.ToString() + ":" + minutes.ToString();
+                currentTime = hours + (minutes / 100);
+            }
+            else
+            {
+                clockValue.text = hours.ToString() + ":" + minutes.ToString();
+            }
+        }
+        else
+        {
+            Debug.Log("is INT");
+            hours = (int)currentTime;
+            clockValue.text = hours.ToString() + ":00";
+        }
     }
 }
