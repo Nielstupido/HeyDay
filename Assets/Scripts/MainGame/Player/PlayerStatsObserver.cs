8823a7c6a173d1a54ca0a978daec7415ece92aee
Gairus Niel <nielstupidity@gmail.com>
2023-09-27 01:56:01 +0800

    git fix

--- Assets/Scripts/MainGame/Player/PlayerStatsObserver.cs
+++
@@ -1,9 +0,0 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class PlayerStatsObserver : MonoBehaviour
-{
-    public delegate void OnPlayerStatChanged(PlayerStats statName, IDictionary<PlayerStats, float> playerStatsDict);
-    public static OnPlayerStatChanged onPlayerStatChanged;
-}
